c207411c8d8c6da6af24d98e10ef16473fccbdfe
SchallumS <153377514+SchallumS@users.noreply.github.com>
2025-02-18 00:49:19 +0100

    Killmonger
    Add rotation of the space ship in the direction of the joystick define.

--- Assets/Scripts/PlayerControler.cs
+++ Assets/Scripts/PlayerControler.cs
@@ -8,10 +8,18 @@ public class PlayerControler : MonoBehaviour
     [SerializeField] private Rigidbody2D _rigidbody;
     [SerializeField] private FixedJoystick _joystick;
 
-    [SerializeField] private float speed;
+    [SerializeField] private float _speed;
+    [SerializeField] private float _speedRotation;
 
     private void FixedUpdate()
     {
-        _rigidbody.velocity = new Vector2(_joystick.Horizontal * speed, _joystick.Vertical * speed);
+        _rigidbody.velocity = new Vector2(_joystick.Horizontal * _speed, _joystick.Vertical * _speed);
+
+        if (_joystick.Horizontal != 0 || _joystick.Vertical != 0)
+        {
+            Quaternion _targetRotation = Quaternion.LookRotation(transform.forward, _rigidbody.velocity);
+            Quaternion _rotation = Quaternion.RotateTowards(transform.rotation, _targetRotation, _speedRotation * Time.deltaTime);
+            _rigidbody.MoveRotation(_rotation);
+        }
     }
 }
