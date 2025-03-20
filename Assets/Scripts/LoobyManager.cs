c2cfb8aaf89d90dd14637fdc05bfe7d84e48c114
SchallumS <153377514+SchallumS@users.noreply.github.com>
2025-03-20 19:55:06 +0100

    push looby
    looby features

---
+++ Assets/Scripts/LoobyManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.U2D;
+
+
+public class LoobyManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string stage_text = "Stage 01";
+    public Text button_text;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
