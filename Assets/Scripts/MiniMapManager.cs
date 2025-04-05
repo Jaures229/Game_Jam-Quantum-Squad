26c0516ad63bdc04c513aeef9b3de97c27bcaf40
SchallumS <153377514+SchallumS@users.noreply.github.com>
2025-04-05 17:31:11 +0100

    killmonger
    Add cinematic and mini map for stage 2

--- Assets/Scripts/MiniMapManager.cs
+++ Assets/Scripts/MiniMapManager.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+//using System.Numerics;
 using UnityEngine;
 
 public class MiniMapManager : MonoBehaviour
 {
     [SerializeField] private GameObject _circleMarqueur;
+    [SerializeField] private GameObject _vaiseauMarqueur;
     [SerializeField] private List<Transform> _planets;
+    [SerializeField] private Transform _vaiseau;
     [SerializeField] private float _hauteur;
     [SerializeField] private float scale;
     void Start()
     {
+        CreateTriangle(_vaiseau);
         foreach(Transform _planet in _planets)
         {
             CreateCircle(_planet);
@@ -18,7 +22,8 @@ public class MiniMapManager : MonoBehaviour
 
     void Update()
     {
-        
+        transform.position = new Vector3(_vaiseau.transform.position.x, transform.position.y, _vaiseau.transform.position.z
+            );  
     }
 
     private void CreateCircle(Transform _planet)
@@ -29,5 +34,15 @@ public class MiniMapManager : MonoBehaviour
         _circle.transform.localScale = new Vector3(scale, scale, 1f);
 
         _circle.transform.SetParent(_planet);
+    }
+
+    private void  CreateTriangle (Transform _vaiseau)
+    {
+        GameObject _triangle = Instantiate(_vaiseauMarqueur, _vaiseau.position + Vector3.up * _hauteur, Quaternion.Euler(90, 0, 0));
+
+       _triangle.layer = LayerMask.NameToLayer("MiniMapObjects");
+        _triangle.transform.localScale = new Vector3(3000, 4000, 1f);
+
+        _triangle.transform.SetParent(_vaiseau);
     }
 }
