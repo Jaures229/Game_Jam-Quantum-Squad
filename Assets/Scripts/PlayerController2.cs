6aa93bd2fabbe41b1314fc8917dc1cd58d3aad5e
AGOSSOU Jaurès <111900369+Jaures229@users.noreply.github.com>
2025-04-29 18:06:35 +0100

    add transparent glass, resolve falling player

--- Assets/Scripts/PlayerController2.cs
+++ Assets/Scripts/PlayerController2.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PlayerController2 : MonoBehaviour
 {
     [SerializeField] private Joystick joystick;
     [SerializeField] private float speed = 5f;
     [SerializeField] private Animator _animator;
+    NavMeshAgent _agent;
     //[SerializeField] private Animator animator;
 
     private Rigidbody rb;
@@ -16,9 +18,9 @@ public class PlayerController2 : MonoBehaviour
         rb = GetComponent<Rigidbody>();
     }
 
-    void FixedUpdate()
+   /* void FixedUpdate()
     {
-        
+
         Vector3 direction = new Vector3(joystick.Horizontal, 0, joystick.Vertical);
         rb.MovePosition(rb.position + direction * speed * Time.fixedDeltaTime);
 
@@ -28,9 +30,29 @@ public class PlayerController2 : MonoBehaviour
             rb.rotation = Quaternion.Slerp(rb.rotation, targetRotation, 5f * Time.fixedDeltaTime);
         }
         _animator.SetBool("Walk", direction.magnitude > 0.1f);
+    }*/
+
+    void FixedUpdate()
+    {
+        Vector3 direction = new Vector3(joystick.Horizontal, 0, joystick.Vertical);
+
+        // Déplacement
+        rb.MovePosition(rb.position + direction * speed * Time.fixedDeltaTime);
+
+        // Rotation uniquement si on se déplace
+        if (direction.magnitude > 0.1f)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(direction.normalized, Vector3.up);
+            rb.rotation = Quaternion.Slerp(rb.rotation, targetRotation, 2f * Time.fixedDeltaTime);
+        }
+
+        // Animation
+        _animator.SetBool("Walk", direction.magnitude > 0.1f);
     }
 
     private void UpdateAnimation()
     {
     }
 }
+
+
