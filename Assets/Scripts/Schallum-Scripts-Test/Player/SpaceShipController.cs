db766161eb8fa1f0f90637392c36d97e3983f3f8
LuciusKs <lucius.kassa01@gmail.com>
2025-08-10 23:46:05 +0100

    Lucius's work

---
+++ Assets/Scripts/Schallum-Scripts-Test/Player/SpaceShipController.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class SpaceShipController : MonoBehaviour
+{
+    private Rigidbody _spaceShip;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
